0481aef6a677fb533a501c3794290e81e9c794b6
EVAditya <adityasmbox@gmail.com>
2025-04-06 19:52:50 +0530

    worstshit

--- pythoncodes/hardcoder.py
+++ pythoncodes/hardcoder.py
@@ -6,11 +6,11 @@ def memloader(readfile,writefile, start):
                 # Read the entire file
                 lines = file1.readlines()  # Changed from readline() to readlines()
                 for i in range(0,start):
-                    oneline = f"mem[{i}] <= 8'h00\n"  # initializing the memory before start to zero
+                    oneline = f"mem[{i}] <= 8'h00;\n"  # initializing the memory before start to zero
                     file2.writelines(oneline)
                 for i in range(start,256):
                     game=lines[i-start].strip()
-                    oneline = f"mem[{i}] <= 8'h{game}\n" if(game) else f"mem[{i}] <= 8'h00\n"
+                    oneline = f"mem[{i}] <= 8'h{game};\n" if(game) else f"mem[{i}] <= 8'h00;\n"
                     file2.writelines(oneline)
     except Exception as e:
         print(f"An error occurred: {e}")
